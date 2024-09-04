d2872ef7ca27e49ecca77a81e74c787e323b5e65
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-04 23:38:44 +0300

    Add api response domain model

---
+++ Models/ApiResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopLineWebApplication.Models
+{
+    public class ApiResponse
+    {
+        public List<Product> Products { get; set; }
+        public int Total { get; set; }
+        public int Skip { get; set; }
+        public int Limit { get; set; }
+    }
+}
