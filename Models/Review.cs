3753d83404a63bef8adf3da7fea54a798e807ad0
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-04 23:38:24 +0300

    Add review domain model

---
+++ Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopLineWebApplication.Models
+{
+    public class Review
+    {
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+        public DateTime Date { get; set; }
+        public string ReviewerName { get; set; }
+        public string ReviewerEmail { get; set; }
+    }
+}
