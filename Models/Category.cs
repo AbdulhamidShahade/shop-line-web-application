a6b5c1f6db5bc9d45de5f4f2ca699b0fd416bb5b
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-04 23:38:00 +0300

    Add category domain model

---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopLineWebApplication.Models
+{
+    public class Category
+    {
+        public string Slug { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
