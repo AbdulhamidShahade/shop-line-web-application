496b99161f9b4de365633764d60ba7e37034cfb6
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-04 23:45:19 +0300

    Update index funtion to retrive categories data

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
-using ShopLineWebApplication.Models;
-using System.Diagnostics;
+using ShopLineWebApplication.Services;
 
 namespace ShopLineWebApplication.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ICategoryService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ICategoryService productService)
         {
-            _logger = logger;
+            _productService = productService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var categories = await _productService.GetProductCategoriesAsync();
+            return View(categories);
         }
     }
 }
