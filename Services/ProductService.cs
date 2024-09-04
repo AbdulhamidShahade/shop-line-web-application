071b36e4493eb03c5dc3ef7c4a13d74b0a79ada8
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-04 23:41:38 +0300

    Add ProductService.cs

---
+++ Services/ProductService.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Newtonsoft.Json;
+using ShopLineWebApplication.Models;
+
+namespace ShopLineWebApplication.Services
+{
+    public class ProductService : IProductService
+    {
+        private HttpClient _httpClient;
+        private string _url;
+
+        public ProductService(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _url = configuration.GetValue<string>("ApiUrls:ProductsUrl");
+        }
+
+        public async Task<ApiResponse> GetProductsAsync(string point)
+        {
+            var response = await _httpClient.GetStringAsync($"{_url}/{point}");
+
+            ApiResponse apiResponse = JsonConvert.DeserializeObject<ApiResponse>(Convert.ToString(response));
+
+            return apiResponse;
+        }
+    }
+}
