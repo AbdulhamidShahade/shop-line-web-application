f554ab127c3848f9ae597ce11b08aad049593f3a
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-04 23:41:31 +0300

    Add IProductService.cs

---
+++ Services/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using ShopLineWebApplication.Models;
+
+namespace ShopLineWebApplication.Services
+{
+    public interface IProductService
+    {
+        Task<ApiResponse> GetProductsAsync(string point);
+    }
+}
