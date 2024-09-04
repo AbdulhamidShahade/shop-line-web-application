29eeca253f0e44aadab8b515d97f092be9aeac9a
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-04 23:41:15 +0300

    Add ICategoryService.cs

---
+++ Services/ICategoryService.cs
@@ -0,0 +1,9 @@
+﻿using ShopLineWebApplication.Models;
+
+namespace ShopLineWebApplication.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> GetProductCategoriesAsync();
+    }
+}
